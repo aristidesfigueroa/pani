9c6f605f0f37b001edec44049c93003263a0aac7
Domingo <aristides@itsbyte.com>
2020-11-15 15:10:57 -0600

    V.1.4 Con Imagen

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { ActivityIndicator, StyleSheet, Text, View } from 'react-native';
+import { ActivityIndicator, StyleSheet, Text, View, ScrollView, Image } from 'react-native';
 
 export default class App extends React.Component {
 
@@ -8,7 +8,10 @@ export default class App extends React.Component {
     super(props);
     this.state = {
       isLoading : true,
-      dataSource: null,
+      _sorteo: "",
+      _fecha_sorteo: "",
+      _vencimiento_sorteo: "",
+      _premios_menor: [],
     }
   }
 
@@ -20,7 +23,10 @@ export default class App extends React.Component {
 
       this.setState({
         isLoading: false,
-        dataSource: reponseJson.premios_menor,
+        _sorteo: reponseJson.sorteo,
+        _fecha_sorteo: reponseJson.fecha_sorteo,
+        _vencimiento_sorteo: reponseJson.vencimiento_sorteo,
+        _premios_menor: reponseJson.premios_menor,
       })
 
     })
@@ -42,36 +48,60 @@ export default class App extends React.Component {
     if (this.state.isLoading) {
 
       return (
-        <View style={styles.container}>  
-        <ActivityIndicator />      
-        <StatusBar style="auto" />
-      </View>
+        <ScrollView style={styles.myScroll}>
+          <View style={styles.container}>  
+             <ActivityIndicator />      
+             <StatusBar style="auto" />
+             </View>
+        </ScrollView>
+        
 
       );
       
     }else{
 
-      let premiosMenor = this.state.dataSource.map((val, key) => {
+
+      // let sorteo = this.state._sorteo;
+      // let fechaSorteo = this.state._fecha_sorteo;
+      // let fechaVencimiento = this.state._vencimiento_sorteo;
+
+      let encabezado = 
+       (<View   style={styles.item}>
+           <Text> Sorteo  #: {this.state._sorteo} </Text>  
+           <Text> Juagado  : {this.state._fecha_sorteo} </Text>
+           <Text> Caduciad de Premios  #: {this.state._vencimiento_sorteo} </Text>             
+        </View>
+       );
+
+            
+
+      let premiosMenor = this.state._premios_menor.map((val, key) => {
         return <View key={key}  style={styles.item}>
-                <Text>{val.numero} {val.clasificacion}</Text>   
-                {/* <Text>{val.clasificacion}</Text>             */}
+                <Text>{val.numero} {val.clasificacion}</Text>               
                 </View>
 
       });
 
       return (
-        <View style={styles.container}>
-          {premiosMenor}        
-          <StatusBar style="auto" />
-        </View>
+        <ScrollView style={styles.myScroll}>
+        <Image 
+              source={require("../pani/assets/img/logo-chica.png")}              
+              resizeMode="contain"
+              style={styles.myImage}
+          />
+          <View style={styles.container}>
+            {encabezado}
+            {/* <Text> Sorteo  #: {sorteo}  style={styles.textA}</Text>
+            <Text> Juagado  : {fechaSorteo} style={styles.textA}</Text>
+            <Text> Caducidad de Premios  : {fechaVencimiento} style={styles.textA}</Text> */}
+            {premiosMenor}        
+            <StatusBar style="auto" />
+          </View>
+        </ScrollView>
+        
       );
 
-    }
-
-
-
-
-    
+    }    
 
   }
   
@@ -79,23 +109,45 @@ export default class App extends React.Component {
 
 
 
+
+/**
+ * STYLES
+ */ 
 const styles = StyleSheet.create({
+  myScroll: {
+    marginLeft: 5,
+    marginRight: 5,
+  }, 
+  myImage: {
+    height: 200,    
+    width:"80%",
+    marginBottom:60,                      
+  }, 
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  textA: {
+    fontWeight: "bold",    
+    fontSize: 19,
+    marginBottom: 10,
+    textAlign: "center",     
+  },
   item: {
-    flex: 1,
+    flex: 5,
     alignSelf: "stretch",
     margin: 10,
     alignItems: "center",
     justifyContent: 'center',
     borderBottomWidth: 1,
-    borderBottomColor: "#00a680"
-
-
+    borderBottomColor: "#00a680",
+    marginBottom:40,
+    // fontSize: 19,
+    //     marginBottom: 10,
+    //     textAlign: "center", 
+    
   }
 });
 
