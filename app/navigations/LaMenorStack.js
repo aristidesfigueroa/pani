e174e3039331f53297c52e0cd3c558980b9a7d25
Domingo <aristides@itsbyte.com>
2020-11-24 01:24:17 -0600

    V.1.14 New LaMenorA Alerta

--- app/navigations/LaMenorStack.js
+++ app/navigations/LaMenorStack.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { createStackNavigator } from '@react-navigation/stack';
-import LaMenor from "../screens/LaMenor";
+// import LaMenor from "../screens/LaMenor";
+
+import LaMenorA from "../screens/LaMenorA";
 
 
 const Stack = createStackNavigator();
@@ -11,7 +13,7 @@ export default function LaMenorStack() {
         <Stack.Navigator>
         <Stack.Screen 
         name="lamenor" 
-        component={LaMenor}
+        component={LaMenorA}
         options={{title:"La Chica"}}
          />
       </Stack.Navigator>
